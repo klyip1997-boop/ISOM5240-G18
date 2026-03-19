0b6930b09b98ccc3ddbcad796f2337e15b6b02f9
yklyip <klyip1997@gmail.com>
2026-03-19 21:10:47 +0800

    Update ISOM5250-G18.py

--- ISOM5250-G18.py
+++ ISOM5250-G18.py
@@ -14,7 +14,7 @@ def load_pipelines():
     # Pipeline 2: Your Custom Fine-Tuned Emotion Classifier 
     # REPLACE THIS STRING with your actual Hugging Face username and repo name!
     # Example: "johnsmith/sephora-emotion-classifier"
-    emotion_classifier = pipeline("text-classification", model="YOUR_HF_USERNAME/sephora-emotion-classifier")
+    emotion_classifier = pipeline("text-classification", model="klyipaf/emotion-classifier")
     
     return auto_replier, emotion_classifier
 
